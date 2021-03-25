f8b4bb0013e56e68240c88d4e697a0ebf76552cf
Pablo de la Peña <p@shedcollective.org>
2021-03-25 13:58:36 +0000

    Replace settings with unified settings

---
+++ src/Settings/IpInfo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Nails\GeoIp\Driver\Settings;
+
+use Nails\Common\Helper\Form;
+use Nails\Common\Interfaces;
+use Nails\Common\Service\FormValidation;
+use Nails\Components\Setting;
+use Nails\Factory;
+
+/**
+ * Class IpInfo
+ *
+ * @package Nails\GeoIp\Driver\Settings
+ */
+class IpInfo implements Interfaces\Component\Settings
+{
+    const KEY_ACCESS_TOKEN = 'sAccessToken';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return 'Geo-IP: IPInfo';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function get(): array
+    {
+        /** @var Setting $oAccessToken */
+        $oAccessToken = Factory::factory('ComponentSetting');
+        $oAccessToken
+            ->setKey(static::KEY_ACCESS_TOKEN)
+            ->setType(Form::FIELD_PASSWORD)
+            ->setLabel('Access Token')
+            ->setEncrypted(true)
+            ->setValidation([
+                FormValidation::RULE_REQUIRED,
+            ]);
+
+        return [
+            $oAccessToken,
+        ];
+    }
+}
